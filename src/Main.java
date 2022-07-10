c2ce4a523c03f24965bf4b9b16fc8854f02a5f1b
Igor <avt191540@yandex.ru>
2022-07-10 13:29:27 +0300

    Continue work-2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
+import javax.naming.NameNotFoundException;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        User user1 = new User("Andrey", 20);
+        User user2 = new User("Mihail", 21);
+        User user3 = new User("Boris", 18);
+        User user4 = new User("Igor", 19);
+        User user5 = new User("Igor", 19);
+        User user6 = new User("Nik", 21);
+        User user7 = new User("Bob", 20);
+        List<User> users = Arrays.asList(user1, user2, user3, user4, user5, user6, user7);
+
+        System.out.println(findOldUser(users));
+        System.out.println(findOldUserByStream(users));
+    }
+    private static User findOldUser(List<User> users) {
+        System.out.println(users);
+        Set<User> userSet = new HashSet<>(users);
+        System.out.println(userSet);
+        List<User> userAfterSet = new ArrayList<>(userSet);
+        System.out.println(userAfterSet);
+        Comparator<User> comp = Comparator.comparing(User::getAge).thenComparing(User::getName);
+        Collections.sort(userAfterSet,comp);
+        System.out.println(userAfterSet);
+//        System.out.println(userAfterSet.get(userAfterSet.size() - 1));
+
+        return userAfterSet.get(userAfterSet.size() - 1);
+    }
+
+    private static User findOldUserByStream(List<User> users) {
+        return users.stream().distinct().sorted(Comparator.comparing(User::getAge).thenComparing(User::getName))
+                .skip(users.size() - 2).findFirst().orElseThrow();
     }
 
 }
---
+++ src/User.java
@@ -0,0 +1,59 @@
+import java.util.Objects;
+
+//public class User implements Comparable<User> {
+public class User {
+
+    String name;
+    int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+//    @Override
+//    public int compareTo(User o) {
+//        int resultAge = this.getAge() - o.getAge();
+//        return 0;
+//    }
+}
